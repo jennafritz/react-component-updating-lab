5a8daec7777d191b72e9141162a34f9b29370f91
jennafritz <jennafritz24@gmail.com>
2021-04-29 12:17:28 -0400

    commit tree c3f1a05e9b8bbb5384a1680486c5f311ca2097b6

--- src/Timer.js
+++ src/Timer.js
@@ -23,6 +23,24 @@ class Timer extends Component {
     clearInterval(this.interval);
   }
 
+  //this changes the color of the timer text every time it updates (increases)
+  //however, until we make a shouldComponentUpdate method, the color will ALSO
+  //update every time the parent (App) updates, aka every time the + or - buttons
+  //are pressed
+  componentDidUpdate(){
+    this.timer.current.style.color = "#" + Math.floor(Math.random()*16777215).toString(16)
+  }
+
+  //we can stop this behavior via shouldComponenUpdate (note this method has access
+  //to the next props/state as arguments AND the current props/state as this.props/state):
+  shouldComponentUpdate(nextProps, nextState){
+    if(this.state.time === nextState.time){
+      return false
+    } return true
+    }
+  //ALTERNATIVE to above is to change the Timer component from regular to Pure
+  //then we don't need shouldComponentUpdate bc Pure components have their own version of it
+
   render() {
     const { time, color, logText } = this.state;
     return (
